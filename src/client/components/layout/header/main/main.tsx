4ffe24d4479aefca79064f5003046816ff2b3a36
pikapikamart <itsmelight1@gmail.com>
2022-11-17 15:04:26 +0800

    test

--- src/client/components/layout/header/main/main.tsx
+++ src/client/components/layout/header/main/main.tsx
@@ -10,6 +10,8 @@ import { MainHeaderNavlinks } from "./navlinks"
 import { HeaderSearchbar } from "./searchbar"
 import { MainHeaderWrapper } from "./main.styled"
 import { useHeaderAnimation } from "../header.hook"
+import MainLogoSvg from "@/public/logos/bastion-logo-main.svg"
+import { HeaderLogoWrapper } from "@/styled/shared/header"
 
 
 const Main = () =>{
@@ -25,9 +27,12 @@ const Main = () =>{
       className={`${hideHeaderSticky? "scroll-in scroll-out" : 
                               showHeaderSticky? "scroll-in" : ""}`}>
       <RowCenterBetween as="nav">
-        <HeaderLogo
+        {/* <HeaderLogo
           href="/"
-          src="/logos/bastion-logo-main.svg" />
+          src="/logos/bastion-logo-main.svg" /> */}
+          <HeaderLogoWrapper as="div">
+            <MainLogoSvg />
+          </HeaderLogoWrapper>
         { showDesktopItems && <MainHeaderNavlinks /> }
         <RowEnd>
           { !showDesktopItems && <HeaderControls /> }
