3916ccdada9bb92d446763160740e847893e4b15
pikapikamart <itsmelight1@gmail.com>
2022-11-27 06:53:19 +0800

    change writer's page in the main site to use ssr

--- src/pages/writer/[username].tsx
+++ src/pages/writer/[username].tsx
@@ -1,24 +1,25 @@
 import { DefaultLayout } from "@/components/layout/default";
 import { Writer } from "@/components/main/writer";
+import { FourOhFour } from "@/components/shared/404";
 import { Author } from "@/src/server/controllers/article.controller";
 import { connectDatabase } from "@/src/server/database";
-import {
-  findStaffService, 
-  findManyStaffsService } from "@/src/server/services/staff.service";
-import { wrapper } from "@/store/index";
+import {findStaffService, } from "@/src/server/services/staff.service";
 import { 
-  GetStaticPaths, 
-  GetStaticPropsContext, 
-  InferGetStaticPropsType} from "next";
+  GetServerSidePropsContext,
+  InferGetServerSidePropsType } from "next";
 import { ParsedUrlQuery } from "querystring";
 import { Helmet } from "react-helmet-async";
 import { NextPageWithLayout } from "../_app";
 
 
-const UsernamePage: NextPageWithLayout<InferGetStaticPropsType<typeof getStaticProps>> = ({ author }) =>{
+const UsernamePage: NextPageWithLayout<InferGetServerSidePropsType<typeof getServerSideProps>> = ({ author }) =>{
   
   if ( !author ) {
-    return <></>
+    return (
+      <FourOhFour>
+        Make sure to check the writer's name if correct
+      </FourOhFour>
+    )
   }
 
   return (
@@ -33,36 +34,11 @@ const UsernamePage: NextPageWithLayout<InferGetStaticPropsType<typeof getStaticP
 
 UsernamePage.getLayout = page => DefaultLayout(page)
 
-type Params = {
-  params: {
-    username: string
-  }
-}
-
-export const getStaticPaths: GetStaticPaths = async() =>{
-  await connectDatabase()
-  const writers = await findManyStaffsService({}, "-_id username")
-  const writersParams = writers.reduce((accu, curr) => {
-    accu.push({
-      params: {
-        username: curr.username
-      }
-    })
-
-    return accu
-  }, [] as Params[])
-
-  return {
-    paths: writersParams,
-    fallback: true
-  }
-}
-
 type UsernameParams = ParsedUrlQuery & {
   username: string
 }
 
-export const getStaticProps = wrapper.getStaticProps(store => async( context: GetStaticPropsContext ) =>{
+export const getServerSideProps = async( context: GetServerSidePropsContext ) =>{
   await connectDatabase()
   const { username } = context.params as UsernameParams
   const author = await findStaffService(
@@ -75,7 +51,7 @@ export const getStaticProps = wrapper.getStaticProps(store => async( context: Ge
       author: JSON.parse(JSON.stringify(author)) as Author
     }
   }
-})
+}
 
 
 export default UsernamePage
